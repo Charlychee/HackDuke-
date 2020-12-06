c3f0dc1a790eb942832c09c942112f3aa8d4cc77
Charlychee <charles.lychee@gmail.com>
2020-12-05 20:46:37 -0800

    Added gcloud integration

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import Flask, render_template, redirect, request, url_for, send_from_directory, jsonify
+#from HackDuke import app
+
+app = Flask(__name__)
+
+
+@app.route("/registration", methods=["POST", "GET"])
+def submit_page():
+    if request.method == "POST":
+       name = request.form["name"]
+       dob = request.form["dob"]
+       diagnosis = request.form["diagnosis"]
+       purpose = request.form.getlist("purpose")
+       contact = request.form.getlist("contact")
+       upload = request.files['profilePic']
+    return render_template("registration.html")
+
+@app.route("/match", methods=["POST", "GET"])
+def match_page():
+    return render_template("match.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
