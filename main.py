092e01b25ce3e62e6b3c77ee1734ca461f5f78ee
Charlychee <charles.lychee@gmail.com>
2020-12-06 02:48:11 -0800

    Save picture to folder and adds path to db

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from flask import Flask, render_template, redirect, request, url_for, send_from_
 #from HackDuke import app
 import sqlite3 as sql
 from sqlite import removeUser
+import os
+from werkzeug.utils import  secure_filename
 
 validated = False
 cred = ""
@@ -17,30 +19,36 @@ app = Flask(__name__)
 @app.route("/registration", methods=["POST", "GET"])
 def submit_page():
     if request.method == "POST":
-       email = request.form["email"]
-       pwd = request.form["pwd"]
-       name = request.form["name"]
-       #dob = request.form["dob"]
-       #diagnosis = request.form["diagnosis"]
-       #purpose = request.form.getlist("purpose")
-       #contact = request.form.getlist("contact")
-       #upload = request.files['profilePic']
-       with sql.connect("database.db") as con:
-           cur = con.cursor()
-           cur.execute("INSERT INTO users (email, pwd, name) VALUES (?,?,?)",(email, pwd, name) )
-           con.commit()
+        email = request.form["email"]
+        pwd = request.form["pwd"]
+        name = request.form["name"]
+        file = request.files['profilePic']
+        path = secure_filename(file.filename)
+        full_path = app.instance_path.rstrip("instance") + "profile_pics\\" + path
+        file.save(full_path)
+        #path.save(os.path.join(app.instance_path), "upload", secure_filename(path.filename))
+        #dob = request.form["dob"]
+        #diagnosis = request.form["diagnosis"]
+        #purpose = request.form.getlist("purpose")
+        #contact = request.form.getlist("contact")
+        #upload = request.files['profilePic']
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO users (email, pwd, name, path) VALUES (?,?,?, ?)",(email, pwd, name, full_path))
+            con.commit()
+        print(full_path)
     return render_template("registration.html")
 
 @app.route('/list')
 def list():
-   con = sql.connect("database.db")
-   con.row_factory = sql.Row
-   
-   cur = con.cursor()
-   cur.execute("select * from users")
-   
-   rows = cur.fetchall(); 
-   return render_template("list.html",rows = rows)
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("select * from users")
+
+    rows = cur.fetchall();
+    return render_template("list.html",rows = rows)
 
 
 @app.route("/match", methods=["POST", "GET"])
